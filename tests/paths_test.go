91bd4c3f8a87c2a3c6b088212a2ed3f9bf248757
davodhiambo <jesseomolo@yahoo.com>
2025-01-14 23:03:54 +0300

    Testing: add TestFindPaths.

--- tests/paths_test.go
+++ tests/paths_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"reflect"
 	"testing"
 
 	"lemin/models"
@@ -19,5 +20,26 @@ func TestFindPaths_NoPath(t *testing.T) {
 
 	if len(paths) != 0 {
 		t.Errorf("Expected empty slice, but got %v", paths)
+	}
+}
+
+func TestFindPaths(t *testing.T) {
+	startRoom := "A"
+	endRoom := "C"
+	links := []models.Link{
+		{From: "A", To: "B"},
+		{From: "B", To: "C"},
+	}
+
+	expectedPaths := [][]string{{"A", "B", "C"}}
+
+	paths := utils.FindPaths(startRoom, endRoom, links)
+
+	if len(paths) != len(expectedPaths) {
+		t.Errorf("Expected %d path, but got %d", len(expectedPaths), len(paths))
+	}
+
+	if !reflect.DeepEqual(paths, expectedPaths) {
+		t.Errorf("Expected paths %v, but got %v", expectedPaths, paths)
 	}
 }
