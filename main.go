a82dc2d458d0b24f2a0d865a4dde876867128fb7
rodnochieng <rodneyotieno27@gmail.com>
2025-01-15 14:33:54 +0300

    rmove models from main, send output input printing to utils package(validation abd priting)

--- main.go
+++ main.go
@@ -1,53 +1,11 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
-	"strings"
 
 	"lemin/utils"
 )
-
-type Room struct {
-	name    string
-	x, y    int
-	isStart bool
-	isEnd   bool
-}
-
-type Link struct {
-	from         string
-	to           string
-	NumberofAnts int
-}
-
-func ValidContent(filename string) ([]string, error) {
-	fileContent, err := os.Open(filename)
-	if err != nil {
-		return nil, fmt.Errorf("error reading file: %v", err)
-	}
-	defer fileContent.Close()
-
-	validContent := []string{}
-	scanner := bufio.NewScanner(fileContent)
-
-	for scanner.Scan() {
-		line := strings.TrimSpace(scanner.Text())
-		if line == "" {
-			continue
-		}
-		validContent = append(validContent, line)
-	}
-
-	if err := scanner.Err(); err != nil {
-		return nil, fmt.Errorf("error scanning file: %v", err)
-	}
-
-	return validContent, nil
-}
-
-
 
 func main() {
 	if len(os.Args) != 2 {
@@ -68,7 +26,7 @@ func main() {
 	}
 
 	fmt.Println("Input File Content:")
-	//utils.PrintFileContents(os.Args[1])
+	utils.PrintFileContents(os.Args[1])
 
 	fmt.Println("\nSimulating Ant Movement:")
 	utils.SimulateAntMovement(graph.AllPaths, graph.AntCount)
