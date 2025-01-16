629bf600e49571a46df1372099c5424a6e16a3d8
davodhiambo <jesseomolo@yahoo.com>
2025-01-16 16:53:13 +0300

    testing: add TestSortPaths_SinglePath.

--- tests/ants_test.go
+++ tests/ants_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"reflect"
 	"testing"
 
 	"lemin/utils"
@@ -11,5 +12,16 @@ func TestSortPaths_EmptyArray(t *testing.T) {
 	utils.SortPaths(emptyPaths)
 	if len(emptyPaths) != 0 {
 		t.Errorf("Expected empty array to remain empty, but got length %d", len(emptyPaths))
+	}
+}
+
+func TestSortPaths_SinglePath(t *testing.T) {
+	paths := [][]string{{"A", "B", "C"}}
+	expected := [][]string{{"A", "B", "C"}}
+
+	utils.SortPaths(paths)
+
+	if !reflect.DeepEqual(paths, expected) {
+		t.Errorf("SortPaths() = %v, want %v", paths, expected)
 	}
 }
