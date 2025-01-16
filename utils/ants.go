4c34752a2ec241995ca368f1108abe147f689e98
davodhiambo <jesseomolo@yahoo.com>
2025-01-16 19:25:34 +0300

    chore: remove unecessary functions.

--- utils/ants.go
+++ utils/ants.go
@@ -1,14 +1,6 @@
 package utils
 
-type Ant struct {
-	Id       int
-	PathIdx  int
-	Position int
-}
-
-type Paths [][]string
-
-func MoveAnts(ants int, paths Paths) [][]string {
+func MoveAnts(ants int, paths [][]string) [][]string {
 	return paths
 }
 
@@ -20,24 +12,4 @@ func SortPaths(paths [][]string) {
 			}
 		}
 	}
-}
-
-func (p Paths) Swap(i, j int) {
-	p[i], p[j] = p[j], p[i]
-}
-
-func (p Paths) Len() int {
-	return len(p)
-}
-
-func (p Paths) Less(i, j int) bool {
-	// Sort by the first element of each inner slice
-	// Handle cases where an inner slice might be empty
-	if len(p[i]) == 0 {
-		return true
-	}
-	if len(p[j]) == 0 {
-		return false
-	}
-	return p[i][0] < p[j][0]
 }
