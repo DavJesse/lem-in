8e105dd3aa28c60907ea8171fb42df4b6a1de4b3
Your Name <you@example.com>
2025-01-21 18:06:28 +0300

    feat: check is a room is already visited

--- utils/getpaths.go
+++ utils/getpaths.go
@@ -19,26 +19,34 @@ func GetAllPaths(rooms map[string]*models.ARoom, start, end string) [][]string {
 		path := queue.Remove(queue.Front()).([]string)
 		lastRoom := path[len(path)-1]
 
+		// If we've reached the end room, add the path to the result
 		if lastRoom == end {
 			paths = append(paths, path)
 			continue
 		}
 
+		// Track visited rooms for the current path
 		visited := make(map[string]bool)
-
-		for _, room := range path{
-			visited[room] = true 
+		for _, room := range path {
+			visited[room] = true
 		}
 
+		// Explore neighboring rooms for the next potential path
 		for _, nextRoom := range rooms[lastRoom].Links {
-			if !visited[nextRoom] {
+			// Allow revisiting 'start' and 'end' rooms, but not others
+			if !visited[nextRoom] || nextRoom == start || nextRoom == end {
+				// Create a new path by appending the next room
 				newPath := make([]string, len(path))
 				copy(newPath, path)
 				newPath = append(newPath, nextRoom)
+
+				// Push the new path into the queue for further exploration
 				queue.PushBack(newPath)
 			}
 		}
 	}
+
+	// Return all found paths
 	fmt.Println(paths)
 	return paths
 }
