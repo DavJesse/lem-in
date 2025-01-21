5b66c3f61ba02785789409398c75860e62aecfac
davodhiambo <jesseomolo@yahoo.com>
2025-01-21 04:11:06 +0300

    testing: add TestGetAllPaths_NoValidPaths.

--- tests/paths_test.go
+++ tests/paths_test.go
@@ -1,1 +1,22 @@
 package test
+
+import (
+	"testing"
+
+	"lemin/models"
+	"lemin/utils"
+)
+
+func TestGetAllPaths_NoValidPaths(t *testing.T) {
+	rooms := map[string]*models.ARoom{
+		"A": {Name: "A", Links: []string{"B"}},
+		"B": {Name: "B", Links: []string{"A"}},
+		"C": {Name: "C", Links: []string{}},
+	}
+
+	paths := utils.GetAllPaths(rooms, "A", "C")
+
+	if len(paths) != 0 {
+		t.Errorf("Expected empty slice, but got %v paths", len(paths))
+	}
+}
