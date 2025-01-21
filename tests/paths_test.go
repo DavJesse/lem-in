50424c9d1cf1adb6bac1f293e9af58c5e89a756e
davodhiambo <jesseomolo@yahoo.com>
2025-01-21 04:12:19 +0300

    testing: add TestGetAllPaths_SingleDirectPath.

--- tests/paths_test.go
+++ tests/paths_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"reflect"
 	"testing"
 
 	"lemin/models"
@@ -18,5 +19,24 @@ func TestGetAllPaths_NoValidPaths(t *testing.T) {
 
 	if len(paths) != 0 {
 		t.Errorf("Expected empty slice, but got %v paths", len(paths))
+	}
+}
+
+func TestGetAllPaths_SingleDirectPath(t *testing.T) {
+	rooms := map[string]*models.ARoom{
+		"start": {Links: []string{"end"}},
+		"end":   {Links: []string{"start"}},
+	}
+
+	paths := utils.GetAllPaths(rooms, "start", "end")
+
+	expectedPaths := [][]string{{"start", "end"}}
+
+	if len(paths) != len(expectedPaths) {
+		t.Errorf("Expected %d path, got %d", len(expectedPaths), len(paths))
+	}
+
+	if !reflect.DeepEqual(paths, expectedPaths) {
+		t.Errorf("Expected paths %v, got %v", expectedPaths, paths)
 	}
 }
