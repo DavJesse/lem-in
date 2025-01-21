08ccb8e81a4758a1397f16c76095734b7f62cd0d
davodhiambo <jesseomolo@yahoo.com>
2025-01-21 03:32:16 +0300

    testing: add TestParseInput_NoAnts.

--- tests/parse_input_test.go
+++ tests/parse_input_test.go
@@ -8,7 +8,7 @@ import (
 	"lemin/utils"
 )
 
-func TestParseInputMultiple_EndRooms(t *testing.T) {
+func TestParseInput_MultipleEndRooms(t *testing.T) {
 	// Create a temporary file with multiple end rooms
 	content := `10
 ##start
@@ -47,5 +47,35 @@ room1-end2`
 	expectedError := "invalid data format, multiple end rooms"
 	if err != nil && !strings.Contains(err.Error(), expectedError) {
 		t.Errorf("Expected error message to contain '%s', but got: %v", expectedError, err)
+	}
+}
+
+func TestParseInput_NoAnts(t *testing.T) {
+	// Create a temporary file with no ants specified
+	tmpfile, err := os.CreateTemp("", "test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(tmpfile.Name())
+
+	// Write test data to the file
+	_, err = tmpfile.WriteString("# This is a comment\n##start\nroom1 0 0\n##end\nroom2 1 1\nroom1-room2\n")
+	if err != nil {
+		t.Fatal(err)
+	}
+	tmpfile.Close()
+
+	// Call ParseInput with the temporary file
+	_, err = utils.ParseInput(tmpfile.Name())
+
+	// Check if an error was returned
+	if err == nil {
+		t.Error("Expected an error for input with no ants, but got nil")
+	}
+
+	// Check if the error message is as expected
+	expectedError := "invalid data format, invalid number of ants"
+	if err.Error() != expectedError {
+		t.Errorf("Expected error message '%s', but got '%s'", expectedError, err.Error())
 	}
 }
