5675d6041d0b7d1439dc4258d02520326377ad34
Your Name <you@example.com>
2025-01-22 16:24:14 +0300

    add antmovement

--- tests/antMovement_test.go
+++ tests/antMovement_test.go
@@ -38,3 +38,64 @@ func TestIsValidPath(t *testing.T) {
 		})
 	}
 }
+
+func TestDistributeAnts(t *testing.T) {
+	tests := []struct {
+		name          string
+		paths         [][]string
+		totalAnts     int
+		expectedPaths []utils.PathInfo
+	}{
+		{
+			name:      "Simple test with 3 paths and 6 ants",
+			paths:     [][]string{{"A", "B", "C"}, {"A", "D", "E"}, {"F", "G", "H"}},
+			totalAnts: 6,
+			expectedPaths: []utils.PathInfo{
+				{Path: []string{"A", "B", "C"}, Length: 2, AntsUsing: 2},
+				{Path: []string{"A", "D", "E"}, Length: 2, AntsUsing: 2},
+				{Path: []string{"F", "G", "H"}, Length: 2, AntsUsing: 2},
+			},
+		},
+		{
+			name:      "Test with 2 paths and 5 ants",
+			paths:     [][]string{{"A", "B", "C"}, {"D", "E", "F"}},
+			totalAnts: 5,
+			expectedPaths: []utils.PathInfo{
+				{Path: []string{"A", "B", "C"}, Length: 2, AntsUsing: 2},
+				{Path: []string{"D", "E", "F"}, Length: 2, AntsUsing: 3},
+			},
+		},
+		{
+			name:          "Edge case with 0 paths",
+			paths:         [][]string{},
+			totalAnts:     5,
+			expectedPaths: nil,
+		},
+
+		{
+			name:      "Edge case with 1 path and 5 ants",
+			paths:     [][]string{{"A", "B", "C", "D"}},
+			totalAnts: 5,
+			expectedPaths: []utils.PathInfo{
+				{Path: []string{"A", "B", "C", "D"}, Length: 3, AntsUsing: 5},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := utils.DistributeAnts(tt.paths, tt.totalAnts)
+
+			// Compare result with expected paths
+			if len(result) != len(tt.expectedPaths) {
+				t.Errorf("expected %d paths, got %d", len(tt.expectedPaths), len(result))
+			}
+
+			for i, p := range result {
+				if len(p.Path) != len(tt.expectedPaths[i].Path) || p.AntsUsing != tt.expectedPaths[i].AntsUsing {
+					t.Errorf("for path %v: expected %v, got %v", p.Path, tt.expectedPaths[i], p)
+				}
+			}
+		})
+	}
+}
