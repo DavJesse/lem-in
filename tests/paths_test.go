4ab8a4a994eb7600dfcab4de2e1078adecf3e055
Your Name <you@example.com>
2025-01-22 16:24:25 +0300

    add unit test

--- tests/paths_test.go
+++ tests/paths_test.go
@@ -41,43 +41,6 @@ func TestGetAllPaths_SingleDirectPath(t *testing.T) {
 	}
 }
 
-// func TestGetAllPaths(t *testing.T) {
-// 	rooms := map[string]*models.ARoom{
-// 		"start": {Links: []string{"A", "B"}},
-// 		"A":     {Links: []string{"start", "C", "D"}},
-// 		"B":     {Links: []string{"start", "D"}},
-// 		"C":     {Links: []string{"A", "end"}},
-// 		"D":     {Links: []string{"A", "B", "end"}},
-// 		"end":   {Links: []string{"C", "D"}},
-// 	}
-
-// 	paths := utils.GetAllPaths(rooms, "start", "end")
-// 	log.Printf("Paths: %#v", paths)
-
-// 	expectedPaths := [][]string{
-// 		{"start", "A", "C", "end"},
-// 		{"start", "A", "D", "end"},
-// 		{"start", "B", "D", "end"},
-// 	}
-
-// 	if len(paths) != len(expectedPaths) {
-// 		t.Errorf("Expected %d paths, but got %d", len(expectedPaths), len(paths))
-// 	}
-
-// 	for _, expectedPath := range expectedPaths {
-// 		found := false
-// 		for _, actualPath := range paths {
-// 			if equalPaths(expectedPath, actualPath) {
-// 				found = true
-// 				break
-// 			}
-// 		}
-// 		if !found {
-// 			t.Errorf("Expected path %v not found in the result", expectedPath)
-// 		}
-// 	}
-// }
-
 func equalPaths(path1, path2 []string) bool {
 	if len(path1) != len(path2) {
 		return false
@@ -88,40 +51,6 @@ func equalPaths(path1, path2 []string) bool {
 		}
 	}
 	return true
-}
-
-func TestGetAllPaths_WithCycles(t *testing.T) {
-	rooms := map[string]*models.ARoom{
-		"A": {Name: "A", Links: []string{"B", "C"}},
-		"B": {Name: "B", Links: []string{"A", "D"}},
-		"C": {Name: "C", Links: []string{"A", "D"}},
-		"D": {Name: "D", Links: []string{"B", "C", "E"}},
-		"E": {Name: "E", Links: []string{"D"}},
-	}
-
-	paths := utils.GetAllPaths(rooms, "A", "E")
-
-	expectedPaths := [][]string{
-		{"A", "B", "D", "E"},
-		{"A", "C", "D", "E"},
-	}
-
-	if len(paths) != len(expectedPaths) {
-		t.Errorf("Expected %d paths, but got %d", len(expectedPaths), len(paths))
-	}
-
-	for _, expectedPath := range expectedPaths {
-		found := false
-		for _, actualPath := range paths {
-			if reflect.DeepEqual(expectedPath, actualPath) {
-				found = true
-				break
-			}
-		}
-		if !found {
-			t.Errorf("Expected path %v not found in result", expectedPath)
-		}
-	}
 }
 
 func TestGetAllPaths_SameStartEnd(t *testing.T) {
