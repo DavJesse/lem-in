0f4d4b9859b526d4cd040569e5cb3463d6bbdc60
davodhiambo <jesseomolo@yahoo.com>
2025-01-26 00:55:01 +0300

    testing: add TestMoveAnts_MultiplePaths.

--- tests/move_ants_test.go
+++ tests/move_ants_test.go
@@ -23,3 +23,29 @@ func TestMoveAnts_SinglePathOneAntOneRoom(t *testing.T) {
 		t.Errorf("MoveAnts() = %v, want %v", result, expected)
 	}
 }
+
+func TestMoveAnts_MultiplePaths(t *testing.T) {
+	paths := []models.Path{
+		{
+			Rooms: []string{"A", "B", "end"},
+			Ants:  []string{"1", "2"},
+		},
+		{
+			Rooms: []string{"X", "Y", "Z", "end"},
+			Ants:  []string{"3"},
+		},
+	}
+
+	expected := []string{
+		"L1-A L3-X",
+		"L1-B L3-Y L2-A",
+		"L1-end L3-Z, L2-B",
+		"L3-end, L2-end",
+	}
+
+	result := utils.MoveAnts(paths)
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("MoveAnts() = %#v, want %#v", result, expected)
+	}
+}
