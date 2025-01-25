fb6b3022692b63e480296505ff61ceb02eb94b81
davodhiambo <jesseomolo@yahoo.com>
2025-01-26 00:27:54 +0300

    testing: remove TestMoveAnts_EmptyPaths since function won't be recieving empty paths.

--- tests/move_ants_test.go
+++ tests/move_ants_test.go
@@ -6,11 +6,3 @@ import (
 	"lemin/models"
 	"lemin/utils"
 )
-
-func TestMoveAnts_EmptyPaths(t *testing.T) {
-	paths := []models.Path{}
-	result := utils.MoveAnts(paths)
-	if len(result) != 0 {
-		t.Errorf("Expected empty slice, but got slice with length %d", len(result))
-	}
-}
