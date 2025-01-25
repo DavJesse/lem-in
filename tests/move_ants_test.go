4cce68fe5d2e33e3fc9d9e7d5126b9939de4a05d
davodhiambo <jesseomolo@yahoo.com>
2025-01-26 00:31:23 +0300

    testing: add TestMoveAnts_SinglePathOneAntOneRoom.

--- tests/move_ants_test.go
+++ tests/move_ants_test.go
@@ -1,8 +1,25 @@
 package test
 
 import (
+	"reflect"
 	"testing"
 
 	"lemin/models"
 	"lemin/utils"
 )
+
+func TestMoveAnts_SinglePathOneAntOneRoom(t *testing.T) {
+	paths := []models.Path{
+		{
+			Ants:  []string{"1"},
+			Rooms: []string{"End"},
+		},
+	}
+
+	expected := []string{"L1 - End"}
+	result := utils.MoveAnts(paths)
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("MoveAnts() = %v, want %v", result, expected)
+	}
+}
